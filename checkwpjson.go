109916728cf9b2aa9a631214cf260c859b893bf0
Omid Destan <zarifi.omid@gmail.com>
2023-09-13 12:37:27 +0330

    Check Errors

--- checkwpjson.go
+++ checkwpjson.go
@@ -36,6 +36,7 @@ var outlast []string
 var fo *os.File
 
 func main() {
+	//Receive input from the user.
 	input := flag.String("i", "input.txt", "Input List")
 	output := flag.String("o", "output.txt", "Output List")
 	help := flag.Bool("h", false, "Show help")
@@ -45,13 +46,14 @@ func main() {
 		fmt.Println("-i : To Input File Address")
 		fmt.Println("-o : To OutPut File Address")
 	}
-
+	//Open OutPut File in Directory
 	fo, err := os.OpenFile(*output, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0777)
 	if err != nil {
 
 		fmt.Println(color.Colorize(color.Red, "[-]  Error:"+err.Error()))
 		fmt.Println(color.Colorize(color.Red, "[-] Error In Permission To Open File"))
 	}
+	//To Skip Insecure SSL
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
@@ -63,19 +65,24 @@ func main() {
 		}}
 
 	InputFile := *input
+	//Open Input File
 	InputWebs, err := os.Open(InputFile)
 	if err != nil {
 		fmt.Println(color.Colorize(color.Red, "[-]  Error:"+err.Error()))
 		recover()
 	}
+	//Scan All Line of input file
 	InputBuf := bufio.NewScanner(InputWebs)
 	InputBuf.Split(bufio.ScanLines)
 
 	for InputBuf.Scan() {
+		//Check End Of File Or not
 		InputText := InputBuf.Text()
-
+		//It puts the values of the file line by line into the variable.
 		path := fmt.Sprintf(InputText+"/%s", "wp-json/wp/v2/users")
+		// Add WP-json Directory To your Address
 		req, _ := http.NewRequest("GET", path, nil)
+		//Send Request To Address
 		resp, erer := client.Do(req)
 
 		if erer != nil {
@@ -83,12 +90,14 @@ func main() {
 			continue
 		}
 		if resp.StatusCode == 200 {
+			//Check Response status Code
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
 				fmt.Println(color.Colorize(color.Red, "[-]  Error:"+err.Error()))
 				continue
 			}
 			err = json.Unmarshal(body, &authors)
+			//Parse Json Values
 			if err != nil {
 				fmt.Println(color.Colorize(color.Red, "[-] Line 91 Error:"+err.Error()))
 				continue
