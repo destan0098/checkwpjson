de8ad65f731f67b9a1ccda57989993c7937654cd
Omid Destan <zarifi.omid@gmail.com>
2023-09-13 11:48:59 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module checkpointwpjson
+
+go 1.20
+
+require github.com/TwiN/go-color v1.4.1 // indirect
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/TwiN/go-color"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Author struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	URL         string `json:"url"`
+	Description string `json:"description"`
+	Link        string `json:"link"`
+	Slug        string `json:"slug"`
+	AvatarURLs  struct {
+		Size24 string `json:"24"`
+		Size48 string `json:"48"`
+		Size96 string `json:"96"`
+	} `json:"avatar_urls"`
+	Meta []interface{} `json:"meta"`
+	ACF  []interface{} `json:"acf"`
+	// Add more fields here as needed
+}
+
+var authors []Author
+var outlast []string
+
+//func errormanager(err error) {
+//	fmt.Println("[-] " + err.Error())
+//
+//}
+
+var fo *os.File
+
+func main() {
+
+	fo, err := os.OpenFile("out.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0777)
+	if err != nil {
+
+		fmt.Println(color.Colorize(color.Red, "[-] Line 46 Error:"+err.Error()))
+		fmt.Println(color.Colorize(color.Red, "[-] Error In Permission To Open File"))
+	}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	// Do not verify certificates, do not follow redirects.
+	client := &http.Client{
+		Transport: tr,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}}
+
+	Payloadfile := "checkpointvalid.txt"
+	Payload, err := os.Open(Payloadfile)
+	if err != nil {
+		fmt.Println(color.Colorize(color.Red, "[-] Line 62 Error:"+err.Error()))
+		recover()
+	}
+	PayloadBuf := bufio.NewScanner(Payload)
+	PayloadBuf.Split(bufio.ScanLines)
+
+	for PayloadBuf.Scan() {
+		Payl := PayloadBuf.Text()
+		path := fmt.Sprintf(Payl+"/%s", "wp-json/wp/v2/users")
+		req, _ := http.NewRequest("GET", path, nil)
+		resp, erer := client.Do(req)
+
+		if erer != nil {
+			fmt.Println(color.Colorize(color.Red, "[-] Line 75 Error:"+erer.Error()))
+			continue
+		}
+		if resp.StatusCode == 200 {
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println(color.Colorize(color.Red, "[-] Line 81 Error:"+err.Error()))
+				continue
+			}
+			err = json.Unmarshal(body, &authors)
+			if err != nil {
+				fmt.Println(color.Colorize(color.Red, "[-] Line 86 Error:"+err.Error()))
+				continue
+			}
+			fmt.Println(color.Colorize(color.Green, "[+] Find In : "+path))
+			outlast = append(outlast, path+"\n")
+			for _, author := range authors {
+				outlast = append(outlast, " User Name :\n"+author.Slug+"\n")
+				fmt.Println(color.Colorize(color.Green, "[+] UserNames : "+author.Slug))
+
+			}
+			outlast = append(outlast, "********************************************\n")
+
+			_, err = fmt.Fprint(fo, outlast)
+			if err != nil {
+				fmt.Println(color.Colorize(color.Red, "[-] Line 100 Error:"+err.Error()))
+				continue
+			}
+		}
+	}
+
+}
